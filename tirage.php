e0bd1fd61b234ff5dec1bf1b9f6a469fa41b6138
Noidkoko <marvinguillot@hotmail.fr>
2022-09-12 16:37:49 +0200

    Update tirage.php

--- tirage.php
+++ tirage.php
@@ -4,19 +4,17 @@ $tirages = [];
 for ($i = 0; $i < 6; $i++) {
     do {
         $tr = mt_rand(1, 50);
-    } while (in_array($tr, $tirage));
-    $tirage[] = $tr;
+    } while (in_array($tr, $tirages));
+    $tirages[] = $tr;
 }
 
 $bon = 0;
 for ($i = 0; $i < 6; $i++) {
-    if (in_array($tirage[$i], $selection)) {
+    if (in_array($tirages[$i], $selection)) {
         $bon++;
     }
 }
 ?> 
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -39,9 +37,25 @@ for ($i = 0; $i < 6; $i++) {
     <h2>Resultats du tirage</h2>
     <div id="grid">
         <?php for ($i = 0; $i < 6; $i++) { ?>
-        <input type="button" value="<?= $tirages[$i] ?>" />
+        <input type="button" id="<?= $i ?>" value="<?= $tirages[$i] ?>" />
         <?php } ?>
     </div>
     <h2>Vous avez eu <?= $bon ?> bon(s) numéros</h2>
+    <script>
+        document.body.onload=function() {
+            num="<?php echo implode(' ', $selection); ?>".split(" ");
+            res="<?php echo implode(' ', $tirages); ?>".split(" ");
+            i=0;
+            j=0;
+            tirer();
+        }
+        function tirer() {
+            t=setTimeout("tirer()", 40);
+            if (j<res[i]) {
+                j++;
+                document.getElementById(i).innerHTML = j;
+            }
+        }
+    </script>
 </body>
 </html>
